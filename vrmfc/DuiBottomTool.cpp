2f7cfe8a993e02bd253b013a15f4a7db057b227b
win10_i5-4590 <1281261856@qq.com>
2017-06-10 00:14:59 +0800

    show bottom tool ok

--- vrmfc/DuiBottomTool.cpp
+++ vrmfc/DuiBottomTool.cpp
@@ -1,6 +1,8 @@
 #include "stdafx.h"
 #include "DuiBottomTool.h"
 
+using DuiLib::CHorizontalLayoutUI;
+using DuiLib::CButtonUI;
 
 DUI_BEGIN_MESSAGE_MAP(CDuiBottomTool, CNotifyPump)
 DUI_ON_MSGTYPE(DUI_MSGTYPE_CLICK, OnClick)
@@ -18,6 +20,21 @@ CDuiBottomTool::~CDuiBottomTool()
 
 void CDuiBottomTool::InitWindow()
 {
+	
+	auto container = static_cast<CHorizontalLayoutUI*>(m_PaintManager.FindControl(L"container")); assert(container);
+	if (!container) { return; }
+	int w = container->GetWidth();
+	int h = container->GetHeight();
+
+	const int BTN_W = 64;
+
+	/*SIZE sz = { 5, 0 };
+	auto btn = new CButtonUI();
+	btn->SetBkImage(L"image\\exit.png");
+	btn->SetFixedXY(sz);
+	btn->SetFixedWidth(BTN_W);
+	btn->SetFixedHeight(BTN_W);
+	container->Add(btn);*/
 }
 
 void CDuiBottomTool::Notify(DuiLib::TNotifyUI & msg)
